87f28249e9615402fc660d3fdd65e83ec47d3a64
박준홍 <junhq1234@cu.ac.kr>
2023-03-22 21:58:18 +0900

    Allow all CORS

--- local_shelter_server/local_shelter_server/settings.py
+++ local_shelter_server/local_shelter_server/settings.py
@@ -55,6 +55,8 @@ MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware'
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000'
                          ,'http://localhost:3000'
                          ,'http://localhost:3001']
